292f45f63db3da7b070a3edd71da6ca1fb7ab2da
Vinay <vinay.s@engees.in>
2025-09-23 15:56:21 +0530

    view problem

--- app.js
+++ app.js
@@ -11,14 +11,14 @@ const flowRoutes = require('./routes/flow');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
 mongoose.connect(process.env.MONGODB_URL)
 
