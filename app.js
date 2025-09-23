de504c1a632a85813cff0a32d9f56861c3764dbc
Vinay <vinay.s@engees.in>
2025-09-23 14:37:48 +0530

    changes

--- app.js
+++ app.js
@@ -11,8 +11,8 @@ const flowRoutes = require('./routes/flow');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
