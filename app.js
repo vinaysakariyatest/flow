ab8b43b4b87a39ec54f4b5efccdb07a6ecb4300b
Vinay <vinay.s@engees.in>
2025-09-23 17:41:29 +0530

    change in app.js

--- app.js
+++ app.js
@@ -30,20 +30,16 @@ mongoose.connection.once('open', () => {
 
 app.use('/', flowRoutes);
 
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  next(createError(404));
-});
+// app.use(function (req, res, next) {
+//   next(createError(404));
+// });
 
-// error handler
-app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+// app.use(function (err, req, res, next) {
+//   res.locals.message = err.message;
+//   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+//   res.status(err.status || 500);
+//   res.render('error');
+// });
 
 module.exports = app;
